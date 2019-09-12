1f14956e396a707a752a8ebc3a2490e560e6aba8
Chris Lenart <clenart4@kent.edu>
2019-09-12 18:12:26 -0400

    added index

--- server.py
+++ server.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = 'KHECTFh4ck3rMan'
 
+@app.route("/")
+def index():
+	return "not authorized"
+
 @app.route("/challenge-1")
 def c1():
 	if("curl" in  request.headers.get('User-Agent')):
